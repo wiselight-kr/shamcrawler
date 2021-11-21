bd66cf30dfb53c8c91341e4797acdc152a55635b
wise1ight <59006393+wise1ight@users.noreply.github.com>
2021-11-21 19:39:13 +0900

    Update method for fetch coin list

--- coin/tasks.py
+++ coin/tasks.py
@@ -27,46 +27,27 @@ def updateMarkCap():
 
 @shared_task
 def updateCoin():
-    print('나들어왔어!!')
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    driver = webdriver.Chrome('./chromedriver', chrome_options=chrome_options)
-    print('driver set')
     from coin.models import Coin
 
-    lastPage = getLastPage()
-    PAGEURL = 'https://coinmarketcap.com/?page='
+    start = 1
+    limit = 1000
+    API_URL = 'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/listing?start={}&limit={}&sortBy=market_cap&sortType=desc&convert=USD&cryptoType=all&tagType=all&audited=false&aux=null'
+
     COINS = set()
-    for i in range(1, lastPage + 1):
+
+    first_page = requests.get(API_URL.format(start, limit))
+    first_page_json = json.loads(first_page.text)
+    total_count = first_page_json['data']['totalCount']
+
+    for i in range(1, int(int(total_count) / limit) + 2):
+        page = requests.get(API_URL.format(start, limit))
+        page_json = json.loads(page.text)
+
+        COINS.update(set(map(lambda x: x['slug'], page_json['data']['cryptoCurrencyList'])))
+
+        start += limit
         time.sleep(1)
-        driver.get(PAGEURL + str(i))
-        height = driver.execute_script("return document.body.scrollHeight")
-        toHeight = 0
-        steps = 30
-        heightByStep = height / steps
-        for i in range(steps):
-            time.sleep(1)
-            driver.execute_script("window.scrollTo(0, %d);" % toHeight)
-            toHeight += heightByStep
-        soup = BeautifulSoup(driver.page_source, 'html.parser')
-        tbody = soup.select(
-            '#__next > div > div.main-content > div.sc-57oli2-0.comDeo.cmc-body-wrapper > div > div:nth-child(1) > div.h7vnx2-1.bFzXgL > table > tbody')
-        coins = tbody[0].select('tr > td > div > a')
 
-        for coin in coins:
-            tmp = coin['href'].split('/')[2]
-            try:
-                Coin.objects.get(name=tmp)
-                print('in', tmp)
-            except:
-                print('not in', tmp)
-                COINS.add(tmp)
-            # COINS.add(coin['href'].split('/')[2])
-        print(len(COINS))
-
-    print(len(COINS))
     for coin in COINS:
         Coin.objects.create(name=coin)
     print(len(COINS))
